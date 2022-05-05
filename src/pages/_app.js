dca98ef3af98068fac5ab6f5814b35f539258f7b
Carlos && Ponce <srtager555@gmail.com>
2022-05-04 19:36:33 -0600

    files added for next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import { useState, useEffect, useRef } from 'react';
+
+import { useInPath } from '@hooks/useInPath';
+import { useScrollPosition } from '@hooks/useScrollPosition/useScrollPosition';
+
+import { SlideLetters } from '@components/SlideLetters';
+import { Navbar } from '@components/Navbar';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const sideNavRef = useRef(null);
+  const scrollPosition = useScrollPosition();
+
+  const [sLettersOpts, setSLettersOpts] = useState({});
+  const [inPath, setInPath] = useState('');
+
+  function handleChangePath() {
+    useInPath(setInPath);
+  }
+
+  return (
+    <>
+      <Navbar path={inPath} scrollPosition={scrollPosition} sideNavRef={sideNavRef} />
+      <SlideLetters sLettersOpts={sLettersOpts} scrollPosition={scrollPosition} />
+      <Component
+        sideNavRef={sideNavRef}
+        scrollPosition={scrollPosition}
+        setSLettersOpts={setSLettersOpts}
+        inPath={inPath}
+        callback={() => handleChangePath()}
+        {...pageProps}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
