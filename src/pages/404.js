b2e4528a09a86c3231dbc013e187b7f6cd9c10ed
Carlos && Ponce <srtager555@gmail.com>
2022-05-06 00:38:07 -0600

    404 page not found moved to next js

---
+++ src/pages/404.js
@@ -0,0 +1,5 @@
+import NotFound from '@templates/notFound';
+
+export default function NotFoundPage({ setSLettersOpts }) {
+  return <NotFound setSLettersOpts={setSLettersOpts} />;
+}
--- src/templates/notFound/index.js
+++ src/templates/notFound/index.js
@@ -1,28 +1,30 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from 'react';
+import Link from 'next/link';
 
-import "@Styles/NotFound.css"
+import style from '@styles/NotFound.module.scss';
 
 export default function NotFound({ setSLettersOpts }) {
-   useEffect(() => {
-      setSLettersOpts({ 
-          Slide1: [true, false], 
-          Slide2: [true, false],
-          Slide3: [true, false],
-          Slide4: [true, false],
-          Slide5: [true, false],
-          Slide6: [true, false]
-        });
-   }, []);
-   return (
-      <div className="notFound_container">
-         <div className="notFound__container-404">
-            <span className="notFound__container-404__404">404</span>
-            <span className="notFound__container-404__notMatch">not match</span>
-         </div>
-         <div className="notFound__container-links">
-            <Link to="/">Return to Home </Link>
-         </div>
+  useEffect(() => {
+    setSLettersOpts({
+      Slide1: [true, false],
+      Slide2: [true, false],
+      Slide3: [true, false],
+      Slide4: [true, false],
+      Slide5: [true, false],
+      Slide6: [true, false],
+    });
+  }, []);
+  return (
+    <div className={style.container__main}>
+      <div className={style["container__main--404"]}>
+        <span className={style["container__main--404--404"]}>404</span>
+        <span className={style["container__main--404--notMatch"]}>not match</span>
       </div>
-   );
+      <div className={style["container__main--links"]}>
+        <Link href="/">
+          <a>Return to Home</a>
+        </Link>
+      </div>
+    </div>
+  );
 }
