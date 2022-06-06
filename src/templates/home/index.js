a4d9c69aa5364d78ef2cfc939dae5241870f5721
Carlos && Ponce <srtager555@gmail.com>
2022-06-05 19:58:32 -0600

    code commented and outService page added

--- src/templates/home/index.js
+++ src/templates/home/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import { OutOfService } from "./outOfService";
+
 import { HomeSwitch } from "@components/HomeSwitch";
 import { arrContent } from "@context/HomeContentList";
 import { HomeContentContainer } from "@components/HomeContentContainer";
@@ -7,97 +9,96 @@ import { HomeContentContainer } from "@components/HomeContentContainer";
 import styleHomePage from "@styles/HomePage.module.scss";
 
 export function Home({ setSLettersOpts = {}, scrollPosition, callback }) {
-   const [colorCounter, setColorCounter] = useState(0);
-   const [colorArr, setColorArr] = useState([]);
-   const lettersState1 = {
-      Slide1: [true, true],
-      Slide2: [true, true],
-      Slide3: [false, true],
-      Slide4: [false, true],
-      Slide5: [false, true],
-      Slide6: [false, true],
-   };
-   const lettersState2 = {
-      Slide1: [true, false],
-      Slide2: [true, false],
-      Slide3: [true, false],
-      Slide4: [true, false],
-      Slide5: [true, false],
-      Slide6: [true, false],
-   };
+  const [colorCounter, setColorCounter] = useState(0);
+  const [colorArr, setColorArr] = useState([]);
+  const lettersState1 = {
+    Slide1: [true, true],
+    Slide2: [true, true],
+    Slide3: [false, true],
+    Slide4: [false, true],
+    Slide5: [false, true],
+    Slide6: [false, true],
+  };
+  const lettersState2 = {
+    Slide1: [true, false],
+    Slide2: [true, false],
+    Slide3: [true, false],
+    Slide4: [true, false],
+    Slide5: [true, false],
+    Slide6: [true, false],
+  };
 
-   callback();
+  callback();
 
-   function scrollToTop() {
-      window.scroll(0, 0);
-   }
+  function scrollToTop() {
+    window.scroll(0, 0);
+  }
 
-   useEffect(() => {
-      scrollToTop();
-   }, []);
+  useEffect(() => {
+    scrollToTop();
+  }, []);
 
-   function isResizing() {
-      if (window.innerWidth > 1024) {
-         setSLettersOpts(lettersState2);
-      } else setSLettersOpts({ Slide1: [true, false], Slide2: [true, false] });
-   }
+  function isResizing() {
+    if (window.innerWidth > 1024) {
+      setSLettersOpts(lettersState2);
+    } else setSLettersOpts({ Slide1: [true, false], Slide2: [true, false] });
+  }
 
-   function a() {
-      if (colorCounter === 3) {
-         setColorCounter(0);
-      } else setColorCounter(colorCounter + 1);
-   }
+  function a() {
+    if (colorCounter === 3) {
+      setColorCounter(0);
+    } else setColorCounter(colorCounter + 1);
+  }
 
-   useEffect(() => {
-      setSLettersOpts({
-         Slide1: [true, false],
-         Slide2: [true, false],
-         Slide3: [false, false],
-         Slide4: [false, false],
-         Slide5: [false, false],
-         Slide6: [false, false],
-      });
-      window.addEventListener("resize", isResizing);
-      return () => window.removeEventListener("resize", isResizing);
-   }, []);
+//   useEffect(() => {
+//     setSLettersOpts({
+//       Slide1: [true, false],
+//       Slide2: [true, false],
+//       Slide3: [false, false],
+//       Slide4: [false, false],
+//       Slide5: [false, false],
+//       Slide6: [false, false],
+//     });
+//     window.addEventListener("resize", isResizing);
+//     return () => window.removeEventListener("resize", isResizing);
+//   }, []);
 
-   useEffect(() => {
-      if (
-         scrollPosition >=
-         document.body.clientHeight - window.innerHeight * 1.50
-         ||
-         scrollPosition < window.innerHeight / 10
-      ) {
-         setSLettersOpts(lettersState1);
-         // console.log("a")
-      } else {
-         setSLettersOpts(lettersState2);
-      }
-   }, [scrollPosition]);
+//   useEffect(() => {
+//     if (
+//       scrollPosition >= document.body.clientHeight - window.innerHeight * 1.5 ||
+//       scrollPosition < window.innerHeight / 10
+//     ) {
+//       setSLettersOpts(lettersState1);
+//       // console.log("a")
+//     } else {
+//       setSLettersOpts(lettersState2);
+//     }
+//   }, [scrollPosition]);
 
-   useEffect(() => {
-      if (arrContent.length != colorArr.length) {
-         a();
-         setColorArr([...colorArr, colorCounter]);
-      }
-   }, [colorCounter]);
+  useEffect(() => {
+    if (arrContent.length != colorArr.length) {
+      a();
+      setColorArr([...colorArr, colorCounter]);
+    }
+  }, [colorCounter]);
 
-   const e = arrContent.map((e, index) => {
-      return (
-         <HomeContentContainer
-            component={e[0]}
-            code={e[1]}
-            color={colorArr[index]}
-            key={`container__${index}`}
-         />
-      );
-   });
+  const e = arrContent.map((e, index) => {
+    return (
+      <HomeContentContainer
+        component={e[0]}
+        code={e[1]}
+        color={colorArr[index]}
+        key={`container__${index}`}
+      />
+    );
+  });
 
-   return (
-      <div className={styleHomePage.container__main}>
-         <HomeSwitch />
-         {e}
-         <HomeSwitch bottom={true} />
-      </div>
-   );
+  return (
+    <OutOfService setSLettersOpts={setSLettersOpts} callback={callback} />
+    // <div className={styleHomePage.container__main}>
+    //    <HomeSwitch />
+    //    {e}
+    //    <HomeSwitch bottom={true} />
+    // </div>
+  );
 }
